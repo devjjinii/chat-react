2f46b71b8460182ebd674e9a95cf001d98d8a265
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-10-14 15:10:21 +0900

    [인증] 회원가입 페이지 구현

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function RegisterPage() {
     return (
-        <div>
-            RegisterPage
+        <div className="auth-wrapper">
+            <div style={{textAlign:'center'}}>
+                <h3>가입하기</h3>
+            </div>
+            <form onSubmit = {()=>{}}>
+                <label>Email</label>
+                <input name ="email" type="email" />
+
+                <label>Name</label>
+                <input name ="name" />
+
+                <label>Password</label>
+                <input name ="password" type="password" />
+
+                <label>PassWord Confirm</label>
+                <input name ="password_confirm" type="password" />
+                
+                <input type="submit" />
+
+                <Link style={{color:'gray', textDecoration: 'none'}} to="login">이미 아이디가 있다면</Link>
+            </form>
+
+               
         </div>
     )
 }
